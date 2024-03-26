f2b983d3d9aff41e77117376ef1ecb6b7fec0839
Karl Wachirasorn <127064562+Eshwaldz@users.noreply.github.com>
2024-03-26 21:47:51 +0700

    Update Employee

--- client/src/pages/Theatres/index.js
+++ client/src/pages/Theatres/index.js
@@ -8,17 +8,23 @@ import { Tabs } from "antd";
 import PageTitle from "../../components/PageTitle";
 
 import TheatresList from "./TheatresList";
+import Bookings from "../Profile/Bookings";
 
 function Theatres() {
   return (
     <div>
-      <PageTitle title="Theatres" />
+      <PageTitle title="Employee Control Panel" />
 
       <Tabs defaultActiveKey="1">
 
         <Tabs.TabPane tab="Theatres" key="1">
           <TheatresList />
         </Tabs.TabPane>
+
+        <Tabs.TabPane tab="Bookings" key="2">
+          <Bookings />
+        </Tabs.TabPane>
+
       </Tabs>
     </div>
   );
