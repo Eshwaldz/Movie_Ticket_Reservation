d75f0519f4691bf5a32eec9e79def1b346116d91
Karl Wachirasorn <127064562+Eshwaldz@users.noreply.github.com>
2024-03-31 10:17:09 +0700

    Update Code Document - Login

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -10,17 +10,26 @@ import { LoginUser } from "../../apicalls/users";
 
 import { HideLoading, ShowLoading} from '../../redux/loadersSlice';
 
+/**
+ * Component สำหรับหน้า Login ของเว็บไซต์
+ * @returns {JSX.Element} - โค้ด JSX สำหรับหน้า Login
+ */
 function Login() {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+  const navigate = useNavigate(); // Hook สำหรับการนำทาง
+  const dispatch = useDispatch(); // Hook สำหรับการใช้งาน dispatch
+
+  /**
+   * ฟังก์ชันสำหรับการล็อกอิน
+   * @param {Object} values - ค่าที่รับมาจากฟอร์มการล็อกอิน
+   */
   const onFinish = async (values) => {
     try {
       dispatch(ShowLoading())
-      const response = await LoginUser(values);
+      const response = await LoginUser(values); // เรียกใช้งาน API เพื่อล็อกอิน
       dispatch(HideLoading())
       if (response.success) {
         message.success(response.message);
-        localStorage.setItem("token", response.data);
+        localStorage.setItem("token", response.data); // เก็บ Token ลงใน Local Storage
         window.location.href = "/";
       } else {
         message.error(response.message);
@@ -33,16 +42,17 @@ function Login() {
 
   useEffect(() => {
     if (localStorage.getItem("token")) {
-      navigate("/");
+      navigate("/"); // นำทางไปยังหน้าหลัก หากมี Token อยู่ใน Local Storage
     }
   }, []);
 
   return (
     <div className="flex justify-center h-screen items-center bg-primary">
       <div className="card p-3 w-400">
-        <h1 className="text-xl mb-1">MOVIE - Login</h1>
+        <h1 className="text-xl mb-1">Sign In - Login</h1>
         <hr />
         <Form layout="vertical" className="mt-1" onFinish={onFinish}>
+          {/* Input สำหรับอีเมล */}
           <Form.Item
             label="Email"
             name="email"
@@ -50,6 +60,8 @@ function Login() {
           >
             <input type="email" />
           </Form.Item>
+
+          {/* Input สำหรับรหัสผ่าน */}
           <Form.Item
             label="Password"
             name="password"
@@ -58,7 +70,9 @@ function Login() {
             <input type="password" />
           </Form.Item>
           <div className="flex flex-col mt-2 gap-1">
+            {/* ปุ่มสำหรับล็อกอิน */}
             <Button fullWidth title="Login" type="submit" />
+            {/* ลิงก์สำหรับสมัครสมาชิก */}
             <Link to="/register" className="text-primary">
               {" "}
               Don't have an account?
@@ -71,3 +85,10 @@ function Login() {
 }
 
 export default Login;
+
+/**
+ * Login Component:
+ * 
+ * หน้าที่/การใช้งาน: Login Component ใช้สำหรับแสดงหน้า Login ของเว็บไซต์เพื่อให้ผู้ใช้ทำการล็อกอินเข้าสู่ระบบ.
+ * 
+ */
