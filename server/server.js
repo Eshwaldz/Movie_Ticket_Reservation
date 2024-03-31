95125a1f5b907e657d74db9bbe6bc358bd8859db
Karl Wachirasorn <127064562+Eshwaldz@users.noreply.github.com>
2024-03-31 10:32:23 +0700

    Update Code Document - Server

--- server/server.js
+++ server/server.js
@@ -2,20 +2,26 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 const dbConfig = require('./config/dbConfig');
+
+// เรียกใช้ middleware เพื่อให้ Express สามารถอ่าน JSON ได้
 app.use(express.json());
 
+// เรียกใช้เส้นทางของแต่ละแอพพลิเคชั่น
 const usersRoute = require('./routes/usersRoute');
 const moviesRoute = require("./routes/moviesRoute");
 const theatresRoute = require("./routes/theatresRoute");
 const bookingsRoute = require("./routes/bookingsRoute");
 
+// กำหนดเส้นทางของแต่ละแอพพลิเคชั่น
 app.use('/api/users', usersRoute);
 app.use("/api/movies", moviesRoute);
 app.use("/api/theatres", theatresRoute);
 app.use("/api/bookings", bookingsRoute);
 
+// กำหนดพอร์ตที่ Express จะรันบน
 const port = process.env.PORT || 5000;
 
+// เริ่มต้น Express ให้รอการเชื่อมต่อบนพอร์ตที่กำหนด
 app.listen(port, () =>
   console.log(`Node JS Server is running on port ${port}`)
 );
