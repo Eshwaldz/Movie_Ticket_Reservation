fd3546215910c9e4178d3656b4c7fa70386a3d38
Karl Wachirasorn <127064562+Eshwaldz@users.noreply.github.com>
2024-03-31 10:30:27 +0700

    Update Code Document - dbConfig

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
+
+/**
+ * ตั้งค่าการใช้งานคิวรี่โดยเคร่งความเข้มงวดเป็นเท็จใน Mongoose.
+ */
 mongoose.set('strictQuery', false)
+
+/**
+ * เชื่อมต่อกับฐานข้อมูล MongoDB โดยใช้ URL ที่กำหนดในตัวแปรสิ่งแวดล้อม mongo_url.
+ * @param {string} process.env.mongo_url URL ของ MongoDB
+ * @return {mongoose.Connection} การเชื่อมต่อกับฐานข้อมูล MongoDB
+ */
 mongoose.connect(process.env.mongo_url)
 
 const connection = mongoose.connection;
@@ -12,4 +22,3 @@ connection.on('error' , (err)=>{
     console.log('Mongo DB Failed');
 })
 
-// *******
