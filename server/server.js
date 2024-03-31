e4fc83a7109b830d11c38e33b7cfdc20e67c9958
Karl Wachirasorn <127064562+Eshwaldz@users.noreply.github.com>
2024-03-31 11:14:26 +0700

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -21,6 +21,17 @@ app.use("/api/bookings", bookingsRoute);
 // กำหนดพอร์ตที่ Express จะรันบน
 const port = process.env.PORT || 5000;
 
+const path = require("path");
+__dirname = path.resolve();
+
+// render deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // เริ่มต้น Express ให้รอการเชื่อมต่อบนพอร์ตที่กำหนด
 app.listen(port, () =>
   console.log(`Node JS Server is running on port ${port}`)
